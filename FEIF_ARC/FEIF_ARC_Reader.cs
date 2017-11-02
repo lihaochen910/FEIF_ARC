4f92b9388d2712b4755550ce143c5f5f11355410
Kanbaru <lihaochen910@hotmail.com>
2017-11-02 16:09:37 +0800

    拆分arc解包打包功能 添加arc文件读取类

---
+++ FEIF_ARC/FEIF_ARC_Reader.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FEIF_ARC
+{
+    class FEIF_ARC_Reader : IDisposable
+    {
+#region Static Function
+        /// <summary>
+        /// 获取arc归档中的指定文件数据
+        /// </summary>
+        /// <param name="arcPath">arc文件路径</param>
+        /// <param name="filename">arc归档中的文件</param>
+        /// <returns></returns>
+        public static byte[] GetFileData(string arcPath, string filename)
+        {
+            byte[] archive = File.ReadAllBytes(arcPath);
+
+            return GetFileData(archive, filename);
+        }
+
+        public static byte[] GetFileData(byte[] archive, string filename)
+        {
+            //MetaOffset 0x4
+            uint MetaOffset = BitConverter.ToUInt32(archive, 4) + 0x20;
+            //FileCount 0x8
+            uint FileCount = BitConverter.ToUInt32(archive, 0x8);
+            //In case we're using awakening archive type
+            bool awakening = (BitConverter.ToUInt32(archive, 0x20) != 0);
+
+            for (var i = 0; i < FileCount; i++)
+            {
+                int FileMetaOffset = 0x20 + BitConverter.ToInt32(archive, (int)MetaOffset + 4 * i);
+
+                int FileNameOffset = BitConverter.ToInt32(archive, FileMetaOffset) + 0x20;
+
+                string filename_in_arc = Encoding.UTF8.GetString(archive.Skip(FileNameOffset).TakeWhile(b => b != 0).ToArray());
+
+                if (filename_in_arc != filename)
+                    continue;
+
+                int FileIndex = BitConverter.ToInt32(archive, FileMetaOffset + 4);
+
+                uint FileDataLength = BitConverter.ToUInt32(archive, FileMetaOffset + 8);
+
+                int FileDataOffset = BitConverter.ToInt32(archive, FileMetaOffset + 0xC) + (awakening ? 0x20 : 0x80);
+
+                byte[] filefound = new byte[FileDataLength];
+
+                Array.Copy(archive, FileDataOffset, filefound, 0, FileDataLength);
+
+                return filefound;
+            }
+
+            return null;
+        }
+#endregion
+
+        private uint MetaOffset;
+        private uint FileCount;
+        private bool awakening;
+
+        private byte[] _archive;
+        private Dictionary<string, fileinfo> _fileList;
+        /// <summary>
+        /// 实例一个Arc归档读取器
+        /// </summary>
+        /// <param name="arcPath"></param>
+        public FEIF_ARC_Reader(string arcPath)
+        {
+            _archive = File.ReadAllBytes(arcPath);
+            //MetaOffset 0x4
+            MetaOffset = BitConverter.ToUInt32(_archive, 4) + 0x20;
+            //FileCount 0x8
+            FileCount = BitConverter.ToUInt32(_archive, 0x8);
+            //In case we're using awakening archive type
+            awakening = (BitConverter.ToUInt32(_archive, 0x20) != 0);
+
+            InitFileList();
+        }
+        /// <summary>
+        /// 初始化arc归档中的文件列表
+        /// </summary>
+        private void InitFileList()
+        {
+            _fileList = new Dictionary<string, fileinfo>();
+
+            for (var i = 0; i < FileCount; i++)
+            {
+                int FileMetaOffset = 0x20 + BitConverter.ToInt32(_archive, (int)MetaOffset + 4 * i);
+
+                int FileNameOffset = BitConverter.ToInt32(_archive, FileMetaOffset) + 0x20;
+
+                string filename_in_arc = Encoding.UTF8.GetString(_archive.Skip(FileNameOffset).TakeWhile(b => b != 0).ToArray());
+
+                _fileList.Add(filename_in_arc, new fileinfo(_archive, FileMetaOffset));
+            }
+        }
+        /// <summary>
+        /// 获取arc归档中的指定文件数据
+        /// </summary>
+        /// <param name="filename">文件名</param>
+        /// <returns></returns>
+        public byte[] GetFile(string filename)
+        {
+            if(_fileList.ContainsKey(filename))
+            {
+                byte[] filefound = new byte[_fileList[filename].FileDataLength];
+
+                Array.Copy(_archive, _fileList[filename].FileDataOffset, filefound, 0, _fileList[filename].FileDataLength);
+
+                return filefound;
+            }
+            return null;
+        }
+        /// <summary>
+        /// arc归档中是否存在指定文件
+        /// </summary>
+        /// <param name="filename">指定文件名</param>
+        public bool HasFile(string filename)
+        {
+            return _fileList.ContainsKey(filename);
+        }
+        /// <summary>
+        /// 获取Arc归档中的所有文件
+        /// </summary>
+        /// <returns>(文件名，文件数据)键值对数组</returns>
+        public Dictionary<string, byte[]> GetAllFile()
+        {
+            var allFiles = new Dictionary<string, byte[]>();
+
+            foreach(var file in _fileList)
+            {
+                byte[] filefound = new byte[file.Value.FileDataLength];
+
+                Array.Copy(_archive, file.Value.FileDataOffset, filefound, 0, file.Value.FileDataLength);
+
+                allFiles.Add(file.Key, filefound);
+            }
+
+            return allFiles;
+        }
+        /// <summary>
+        /// 释放该实例占用内存
+        /// </summary>
+        public void Dispose()
+        {
+            _archive = null;
+            _fileList = null;
+        }
+        /// <summary>
+        /// 描述arc归档内的子文件信息
+        /// </summary>
+        struct fileinfo
+        {
+            int FileMetaOffset;
+            public int FileNameOffset;
+            public int FileIndex;
+            public uint FileDataLength;
+            public int FileDataOffset;
+
+            public fileinfo(byte[] archive, int FileMetaOffset)
+            {
+                this.FileMetaOffset = FileMetaOffset;
+
+                FileNameOffset = BitConverter.ToInt32(archive, FileMetaOffset) + 0x20;
+
+                FileIndex = BitConverter.ToInt32(archive, FileMetaOffset + 4);
+
+                FileDataLength = BitConverter.ToUInt32(archive, FileMetaOffset + 8);
+
+                FileDataOffset = BitConverter.ToInt32(archive, FileMetaOffset + 0xC) + ((BitConverter.ToUInt32(archive, 0x20) != 0) ? 0x20 : 0x80);
+            }
+        }
+    }
+}
---
+++ FEIF_ARC/Re_Unpacker.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using GovanifY.Utility;
+
+namespace FEIF_ARC
+{
+    class Re_Unpacker
+    {
+        /// <summary>
+        /// 创建一个arc归档，传入一个文件夹，将文件夹中的所有文件写入arc归档中
+        /// 不支持二级文件夹
+        /// </summary>
+        /// <param name="outdir"></param>
+        /// <param name="newname"></param>
+        public static void CreateFireEmblemArchive(string outdir, string newname)
+        {
+            Console.WriteLine("Creating archive {0}", Path.GetFileName(newname));
+
+            FileStream newfilestream = File.Create(newname);
+
+            //Let's get the number of files
+            //获取文件夹中的所有文件
+            string[] files = Directory.GetFiles(outdir);
+
+            uint FileCount = (uint)files.Length;
+            Console.WriteLine("{0} files detected!", FileCount);
+
+            /**
+             * 使用UTF-8编码写入
+             */
+            //var ShiftJIS = Encoding.GetEncoding(932);
+
+            BinaryStream newFile = new BinaryStream(newfilestream);
+
+            MemoryStream infos = new MemoryStream();
+            BinaryWriter FileInfos = new BinaryWriter(infos);
+
+
+            Console.WriteLine("Creating dummy header...");
+            newFile.Write(0);//Dummy; file size
+
+            //MetaOffset 0x4
+            newFile.Write(0);//dummy should be MetaOffset
+            newFile.Write(FileCount);
+            newFile.Write(FileCount + 3);
+            ///写入0x70个0字节
+            byte nil = 0;
+            for (int i = 0; i < 0x70; i++)
+            {
+                newFile.Write(nil);
+            }
+            ///文件索引
+            int z = 0;
+            ///将文件数据写入
+            foreach (string fileName in files)
+            {
+                Console.WriteLine("Adding file {0}...", Path.GetFileName(fileName));
+                byte[] filetoadd = File.ReadAllBytes(fileName);
+                uint fileoff = (uint)newFile.Tell();
+                newFile.Write(filetoadd);
+                while ((int)newFile.Tell() % 128 != 0)
+                {
+                    newFile.Write(nil);
+                }
+                FileInfos.Write(0);//Name position
+                FileInfos.Write(z);//FileIndex
+                FileInfos.Write(filetoadd.Length);//Length of the file
+                FileInfos.Write(fileoff - 0x80);//Data Offset - 0x80
+                z++;
+            }
+
+            long countinfo = newFile.Tell();
+            newFile.Write(files.Length);//Count is written there afaik
+            long infopointer = newFile.Tell();
+            Console.WriteLine("Adding dummy FileInfos...");
+            /// 移动内存流指针到文件头部
+            /// 写入信息
+            infos.Seek(0, SeekOrigin.Begin);
+            var infopos = newFile.Tell();
+            newFile.Write(infos.ToArray());
+
+            Console.WriteLine("Rewriting header...");
+            long metapos = newFile.Tell();
+            newFile.Seek(4, SeekOrigin.Begin);
+            newFile.Write((uint)metapos - 0x20);
+
+            newFile.Seek(metapos, SeekOrigin.Begin);
+
+            Console.WriteLine("Adding FileInfos pointer...");
+            for (int i = 0; i < files.Length; i++)
+            {
+                newFile.Write((uint)((infopointer + i * 16) - 0x20));
+            }
+
+            Console.WriteLine("Adding Advanced pointers...");
+
+            newFile.Write((uint)0x60);
+            newFile.Write(0);
+            newFile.Write((uint)(countinfo - 0x20));
+            newFile.Write((uint)5);
+            newFile.Write((uint)(countinfo + 4 - 0x20));
+            newFile.Write((uint)0xB);
+            for (int i = 0; i < files.Length; i++)
+            {
+                newFile.Write((uint)((countinfo + 4) + i * 16) - 0x20);
+
+                //Second pointer is a bit more complicated
+                if (i == 0)
+                {
+                    newFile.Write((uint)0x10);
+                }
+                else
+                {
+                    if (i == 1)
+                    {
+                        newFile.Write((uint)0x1C);
+                    }
+                    else
+                    {
+                        newFile.Write((uint)(0x1C + (10 * (i - 1))));//Currently this pointer is unknown, so we assume blindly that a basic pattern is correct
+                    }
+                }
+            }
+
+            //This need to be reversed!
+            //0, 5, 0B, 10, 1C, 26, 30, 3A, 44, 4E
+            //+5, +6, +4, +12, +10, +10, +10, +10, +10
+
+            Console.WriteLine("Adding Filenames...");
+            var datcount = new byte[] { 0x44, 0x61, 0x74, 0x61, 0x00, 0x43, 0x6F, 0x75, 0x6E, 0x74, 0x00, 0x49, 0x6E, 0x66, 0x6F, 0x00 };
+            newFile.Write(datcount);
+            int y = 0;
+
+            foreach (string fileName in files)
+            {
+                FileInfos.Seek(y * 16, SeekOrigin.Begin);
+                long namepos = newFile.Tell();
+                FileInfos.Write((uint)namepos - 0x20);
+                newFile.Write(Encoding.UTF8.GetBytes(Path.GetFileName(fileName)));
+                newFile.Write(nil);
+                y++;
+            }
+            Console.WriteLine("Rewriting FileInfos...");
+            newFile.Seek(infopos, SeekOrigin.Begin);
+
+            infos.Seek(0, SeekOrigin.Begin);
+            newFile.Write(infos.ToArray());
+
+            Console.WriteLine("Finishing the job...");
+            newFile.Seek(0, SeekOrigin.Begin);
+            UInt32 newlength = (UInt32)newFile.BaseStream.Length;
+            newFile.Write(newlength);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Done!");
+            newFile.Close();
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 从一个arc归档解出所有文件
+        /// </summary>
+        /// <param name="outdir"></param>
+        /// <param name="archive"></param>
+        public static void ExtractFireEmblemArchive(string outdir, byte[] archive)
+        {
+            if (Directory.Exists(outdir)) { Directory.Delete(outdir, true); }
+            Directory.CreateDirectory(outdir);
+
+            //Encoding ShiftJIS = Encoding.GetEncoding(932);
+            //使用UTF-8编码
+
+            //MetaOffset 0x4
+            uint MetaOffset = BitConverter.ToUInt32(archive, 4) + 0x20;
+            //FileCount 0x8
+            uint FileCount = BitConverter.ToUInt32(archive, 0x8);
+
+            //In case we're using awakening archive type
+            bool awakening = (BitConverter.ToUInt32(archive, 0x20) != 0);
+
+            Console.WriteLine("Extracting {0} files from {1} to {2}...", FileCount, Path.GetFileName(outdir.Substring(0, outdir.Length - 1)) + ".arc", Path.GetFileName(outdir.Substring(0, outdir.Length - 1)) + "/");
+
+            for (int i = 0; i < FileCount; i++)
+            {
+                int FileMetaOffset = 0x20 + BitConverter.ToInt32(archive, (int)MetaOffset + 4 * i);
+                int FileNameOffset = BitConverter.ToInt32(archive, FileMetaOffset) + 0x20;
+                int FileIndex = BitConverter.ToInt32(archive, FileMetaOffset + 4);
+                uint FileDataLength = BitConverter.ToUInt32(archive, FileMetaOffset + 8);
+                int FileDataOffset = BitConverter.ToInt32(archive, FileMetaOffset + 0xC) + (awakening ? 0x20 : 0x80);
+                byte[] file = new byte[FileDataLength];
+                Array.Copy(archive, FileDataOffset, file, 0, FileDataLength);
+
+                string filename = Encoding.UTF8.GetString(archive.Skip(FileNameOffset).TakeWhile(b => b != 0).ToArray());
+                Console.WriteLine();
+                Console.WriteLine(filename);
+                Console.WriteLine("FileIndex:" + FileIndex);
+                Console.WriteLine("FileDataLength:" + FileDataLength);
+                Console.WriteLine("FileNameOffset:" + FileNameOffset);
+                Console.WriteLine("FileMetaOffset:" + FileMetaOffset);
+
+                string outpath = outdir + Encoding.UTF8.GetString(archive.Skip(FileNameOffset).TakeWhile(b => b != 0).ToArray());
+
+                if (!Directory.Exists(Path.GetDirectoryName(outpath))) { Directory.CreateDirectory(Path.GetDirectoryName(outpath)); }
+                File.WriteAllBytes(outpath, file);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Complete!");
+            Console.ReadKey();
+        }
+    }
+}
